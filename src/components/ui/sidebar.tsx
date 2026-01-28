d7598cc886323a7ec9b6dbcc04eabb3aa809adb1
B P <Binap25@gmail.com>
2026-01-28 22:51:31 +0000

    can you make the screen fixed so the bottom scroll bar is not required

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -213,7 +213,7 @@ const Sidebar = React.forwardRef<
             side={side}
           >
             <SheetHeader>
-              <SheetTitle className="sr-only">Menu</SheetTitle>
+              <SheetTitle>Menu</SheetTitle>
               <SheetDescription className="sr-only">Dashboard navigation</SheetDescription>
             </SheetHeader>
             <div className="flex h-full w-full flex-col">{children}</div>
@@ -332,7 +332,7 @@ const SidebarInset = React.forwardRef<
     <main
       ref={ref}
       className={cn(
-        "relative flex min-h-svh flex-1 flex-col bg-background",
+        "relative flex min-h-svh flex-1 flex-col bg-background min-w-0",
         "peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))] md:peer-data-[variant=inset]:m-2 md:peer-data-[state=collapsed]:peer-data-[variant=inset]:ml-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow",
         className
       )}
@@ -772,3 +772,5 @@ export {
   SidebarTrigger,
   useSidebar,
 }
+
+    
