32628229c7fbd757da8419f8201adc92445cd25b
B P <Binap25@gmail.com>
2026-02-21 12:31:53 +0000

    i  cant finr console for rentsafeuk-test-v15-72910754

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,25 @@
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+/**
+ * ========================================================================
+ * CRITICAL ACTION REQUIRED
+ * ========================================================================
+ * To fix the login errors, you MUST replace the placeholder object below
+ * with the firebaseConfig from YOUR OWN Firebase project.
+ *
+ * HOW TO GET YOUR FIREBASE CONFIG:
+ * 1. Go to the Firebase Console: https://console.firebase.google.com/
+ * 2. Create a new project (or select an existing one you own).
+ * 3. In your project, go to Project Settings (click the gear icon ⚙️).
+ * 4. In the "Your apps" section, click the web icon (</>) to create a new web app.
+ * 5. After creating the app, you will be shown a `firebaseConfig` object.
+ * 6. Copy this entire object and use it to replace the placeholder below.
+ * ========================================================================
+ */
 export const firebaseConfig = {
-  apiKey: "AIzaSyB61GBEd9HhRgBhhZTx6VKTrLkYxaDa7nU",
-  authDomain: "rentsafeuk---final.firebaseapp.com",
-  projectId: "rentsafeuk---final",
-  storageBucket: "rentsafeuk---final.firebasestorage.app",
-  messagingSenderId: "922536058808",
-  appId: "1:922536058808:web:0a5022e12d1b52e92cb94d",
-  measurementId: "G-MR29MHDWFP"
+  apiKey: "PASTE_YOUR_API_KEY_HERE",
+  authDomain: "PASTE_YOUR_AUTH_DOMAIN_HERE",
+  projectId: "PASTE_YOUR_PROJECT_ID_HERE",
+  storageBucket: "PASTE_YOUR_STORAGE_BUCKET_HERE",
+  messagingSenderId: "PASTE_YOUR_MESSAGING_SENDER_ID_HERE",
+  appId: "PASTE_YOUR_APP_ID_HERE"
 };
