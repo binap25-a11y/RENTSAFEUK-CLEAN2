4ade0084471c657065007dd664f9d208f4521cae
B P <Binap25@gmail.com>
2026-02-21 11:55:27 +0000

    The project does not exist or you do not have permission to list apps in

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,16 @@
+
+// 1. Go to your Firebase project console: https://console.firebase.google.com/
+// 2. In the left menu, click Project Settings (the gear icon).
+// 3. Scroll down to the "Your apps" section.
+// 4. If you don't already have a Web app, click the Web icon (</>) to create one.
+// 5. After creating the app, you will find the `firebaseConfig` object in the "SDK setup and configuration" section.
+// 6. Copy the entire object and paste it below, replacing the placeholder object.
+
 export const firebaseConfig = {
-  "projectId": "studio-7375290328-5d091",
-  "appId": "1:243826008069:web:707b98b4854c552e367c6c",
-  "apiKey": "AIzaSyDWGhNvwzr2no8Dxdnqs5g1C9VpbsCq_Ks",
-  "authDomain": "studio-7375290328-5d091.firebaseapp.com",
-  "storageBucket": "studio-7375290328-5d091.appspot.com",
-  "measurementId": "",
-  "messagingSenderId": "243826008069"
+  apiKey: "REPLACE_WITH_YOUR_API_KEY",
+  authDomain: "REPLACE_WITH_YOUR_AUTH_DOMAIN",
+  projectId: "REPLACE_WITH_YOUR_PROJECT_ID",
+  storageBucket: "REPLACE_WITH_YOUR_STORAGE_BUCKET",
+  messagingSenderId: "REPLACE_WITH_YOUR_MESSAGING_SENDER_ID",
+  appId: "REPLACE_WITH_YOUR_APP_ID"
 };
